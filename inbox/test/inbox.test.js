eb1c68b2d1c1613a0520a2ddc7ac133fc160a743
rheeger <rheeger@gmail.com>
2019-02-22 15:38:08 -0800

    battling Rinkeby to get this contract deployed

---
+++ inbox/test/inbox.test.js
@@ -0,0 +1,69 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider())
+const {interface, bytecode} = require('../compile');
+
+
+
+let accounts;
+
+beforeEach(async () => {
+    //get list of accounts
+    accounts = await web3.eth.getAccounts();
+
+    //use one account to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode, arguments:['Hi there!']})
+        .send({ from: accounts[0], gas: '1000000'});
+});
+
+describe('Inbox', ()=> {
+    it('deploys a contract', ()=>{
+        assert.ok(inbox.options.address);
+    });
+
+    it('has a default value', async ()=>{
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hi there!');
+    });
+
+    it('can be updated', async () => {
+        await inbox.methods.setMessage('BAII').send({from:accounts[0], gas: '1000000'})
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'BAII')
+    })
+    
+});
+
+
+
+
+
+
+// ORIGINAL TEST CONTENT NO LONGER NEEDED GOOD REFERENCE THOUGH:
+
+// class Car {
+//     park() {
+//         return 'stopped';
+//     }
+
+//     drive() {
+//         return 'vroom';
+//     }
+// }
+
+// let car;
+
+// beforeEach(()=> {
+//     car = new Car();
+// })
+
+// describe('Car', () =>{
+//     it('can park', () => {
+//         assert.equal(car.park(), 'stopped');
+//     });
+//     it('can drive', () => {
+//         assert.equal(car.drive(), 'vroom');
+//     });
+// });
