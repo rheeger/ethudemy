fa1c228d78a8c2de12d45bead283b307bae47ec3
rheeger <rheeger@gmail.com>
2019-02-25 17:35:09 -0800

    little aside from eth, but starting to get somewhere with react, which is nice

---
+++ 03-react_intro_aside/src/index.js
@@ -0,0 +1,35 @@
+
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search'
+
+import SearchBar from './containers/search_bar'; 
+import VideoList from './containers/video_list'; 
+
+const API_KEY = 'AIzaSyAfTec4hvpROOxvlBaC_shIZUY84eRrU2o';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {videos: []};
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({videos});
+        });
+
+    }
+
+    render(){
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+            );
+    }
+    
+};
+
+ReactDOM.render(<App />, document.querySelector('.container'));
+
