605f454ddbcdbbebfd78b9a5ec560d16d09bef07
rheeger <rheeger@gmail.com>
2019-02-26 12:51:15 -0800

    LUNCH BREAK, lots of cool react tho. this is neat-o, MANNNNNN

--- 03-react_intro_aside/src/index.js
+++ 03-react_intro_aside/src/index.js
@@ -2,7 +2,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
-
+import VideoDetails from './containers/video_detail'
 import SearchBar from './containers/search_bar'; 
 import VideoList from './containers/video_list'; 
 
@@ -12,10 +12,16 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {videos: []};
+        this.state = {
+            videos: [],
+            selectedVideo: null,
+        };
 
         YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({videos});
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0],
+            });
         });
 
     }
@@ -24,7 +30,10 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
-                <VideoList videos={this.state.videos} />
+                <VideoDetails video={this.state.selectedVideo}/>
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
             </div>
             );
     }
